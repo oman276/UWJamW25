c31b96374cc3ea8cb1caadc19a25e4ab11add169
Nicole Planeta <60528262+Eternalon03@users.noreply.github.com>
2025-02-07 23:46:28 -0500

    Aesthetic changes

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -5,9 +5,14 @@ extends Camera2D
 @export var smoothing_speed: float = 5.0
 
 func _process(delta):
-	if target:
-		var target_position = get_node(target).global_position
-		if smoothing_enabled:
-			global_position = global_position.lerp(target_position, smoothing_speed * delta)
-		else:
-			global_position = target_position
+    if target:
+        var target_position = get_node(target).global_position
+        print(target_position)
+        if target_position.y > 450:
+            target_position.y = 450
+        if target_position.y < 275:
+            target_position.y = 275
+        if smoothing_enabled:
+            global_position = global_position.lerp(target_position, smoothing_speed * delta)
+        else:
+            global_position = target_position
