18da495c047888b171796ff6329808a93a1a7c65
Nicole Planeta <60528262+Eternalon03@users.noreply.github.com>
2025-02-09 14:58:57 -0500

    title screen

---
+++ scenes/levels/TitleScreen.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+
+
+func _on_start_button_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton \
+	and event.button_index == MOUSE_BUTTON_LEFT \
+	and event.is_pressed():
+		get_tree().change_scene_to_file("res://scenes/levels/main_scene.tscn")
